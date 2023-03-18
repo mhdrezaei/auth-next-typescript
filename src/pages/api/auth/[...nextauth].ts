0fbc5e97a458f047305fda61122ebffc45c32bf5
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-03-19 01:32:47 +0330

    configure next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { NextPageContext } from "next";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <>
       <h1>welcome</h1>
     </>
   );
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props: {
+      session,
+    },
+  };
 }
