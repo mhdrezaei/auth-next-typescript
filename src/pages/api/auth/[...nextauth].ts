7e3c392b73fa2e0f0678d1c1c15d3c1c913fe4ce
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-21 09:30:55 +0330

    add callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,21 @@
-import NextAuth from "next-auth";
+import NextAuth, { Account, Profile, User } from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from 'next-auth/providers/github'
+// import DiscordProvider from "next-auth/providers/discord";
+// import TwitterProvider from "next-auth/providers/twitter";
+// import Auth0Provider from "next-auth/providers/auth0";
+// import CredentialsProvider from "next-auth/providers/credentials";
+
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
+import { JWT } from "next-auth/jwt";
+import { Adapter, AdapterUser } from "next-auth/adapters";
+
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
@@ -23,5 +31,34 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth",
+  },
+  callbacks: {
+    async jwt({
+      token,
+      user,
+      account,
+      profile,
+      isNewUser,
+    }: {
+      token: JWT;
+      user?: User | Adapter | undefined;
+      account?: Account | null | undefined;
+      profile?: Profile | undefined;
+      isNewUser?: boolean | undefined;
+    }) {
+      if (user) {
+        token.provider = account?.provider;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any; token: JWT }) {
+      if (session.user) {
+        session.user.provider = token.provider;
+      }
+      return session;
+    },
   },
 });
